c24a1901c4ce473f170b3cca5392c9638285a958
Darius Lewis <Glyd@users.noreply.github.com>
2019-04-13 10:20:38 +0100

    modify base to spawn any NPC via ID

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 module.exports = function BrokerAnywhere(dispatch){
     const chatHook = event => {
-        if(!event.message.toLowerCase().includes('!broker')) return
-        
-        dispatch.toClient('S_NPC_MENU_SELECT', 1, {type:28})
+        if(!event.message.toLowerCase().includes('!broker')) {
+            const npcID = event.message.toLowerCase().slice('!broker '); //capture id from chat. no error handling right now
+            const npcTypeObject = {type:npcID}; 
+            return dispatch.toClient('S_NPC_MENU_SELECT', 1, npcTypeObject)
+        }
         return false
     }
     dispatch.hook('C_CHAT', 1, chatHook)
